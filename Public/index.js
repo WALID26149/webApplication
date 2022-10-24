9b20b7f5cc594085361b3c4198860acbfcff7e6a
walid allan <walid.allane@outlook.com>
2022-10-24 11:26:03 +0100

    add the btn to show profile and hide it

--- Public/index.js
+++ Public/index.js
@@ -1,12 +1,15 @@
 const btnDisplay = document.querySelector('.btnDisplay');
 const profile = document.querySelector('.profile');
 const homePage = document.querySelector('.home-page');
+const btnNav = document.querySelector('.navBtn')
 
 btnDisplay.addEventListener('click', function() {
   profile.classList.toggle('hidden');
   homePage.style.width ="100%";
-  // if (document.maxWidth ="1025px") {
-  //   profile.style.display = "block";
-  //   profile.style.display = "none"
-  // }
+
+});
+btnNav.addEventListener('click', function() {
+  if (document.maxWidth ="1025px") {
+    profile.classList.toggle('toggelBetween')
+  }
 });
